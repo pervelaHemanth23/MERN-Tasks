5a0a5afb6214d9d10f9a0e7815427835ff58b244
Pervela Hemanth <pervelahemanthgithub@gmail.com>
2024-03-28 07:15:12 +0530

    Added FIles
    Added all CSS and HTML tasks

---
+++ React/Project1/React/my-app/Components/FuncComp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FuncComp(){
+    return <h1>Hello world, im a functional Component</h1>
+}
+
+export default FuncComp;
---
+++ React/Project1/React/my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FuncComp from '../Components/FuncComp';
+
+function App() {
+  return (
+    <div className="App">
+      <FuncComp />
+    </div>
+  );
+}
+
+export default App;
